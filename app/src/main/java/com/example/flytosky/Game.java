829354b97bd2fb4d96e1fb32660f8fbc0cf696e5
jdniu <601192302@qq.com>
2023-07-08 13:38:07 +0800

    game loop

---
+++ app/src/main/java/com/example/flytosky/Game.java
@@ -0,0 +1,9 @@
+package com.example.flytosky;
+
+import android.view.View;
+
+public class Game extends View {
+    public Game(MainActivity mainActivity) {
+        super();
+    }
+}
---
+++ app/src/main/java/com/example/flytosky/GameLoop.java
@@ -0,0 +1,8 @@
+package com.example.flytosky;
+
+import android.view.SurfaceHolder;
+
+public class GameLoop {
+    public GameLoop(Game game, SurfaceHolder surfaceHolder) {
+    }
+}
